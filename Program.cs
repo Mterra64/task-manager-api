625738f7d6e6c30d3f3adbde1212847eb70cf8ed
MarioTerra_Dev <93275405+Mterra64@users.noreply.github.com>
2026-02-15 18:35:14 -0700

    Add controller services and mapping to application

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
 app.MapControllers();
 
 app.Run();
